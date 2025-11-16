8df99321f6ce2f8b9b45075614e2c71ffc85b3c8
Aashima Khurana <aashimakhurana6@gmail.com>
2025-11-16 18:17:11 +0530

    Add files via upload

---
+++ day25_2.c
@@ -0,0 +1,28 @@
+/*
+Write a program to print the following pattern:
+*****
+ ****
+  ***
+   **
+    *
+*/
+#include <stdio.h>
+
+int main() {
+    int i, j, space;
+
+    // Loop for rows
+    for (i = 5; i >= 1; i--) {
+        // Print leading spaces
+        for (space = 5; space > i; space--) {
+            printf(" ");
+        }
+        // Print stars
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n"); // Move to next line
+    }
+
+    return 0;
+}
---
+++ day26_1.c
@@ -0,0 +1,28 @@
+/*
+Write a program to print the following pattern:
+    5
+   45
+  345
+ 2345
+12345
+*/
+#include <stdio.h>
+
+int main() {
+    int i, j, space;
+
+    // Outer loop for rows
+    for (i = 5; i >= 1; i--) {
+        // Print leading spaces
+        for (space = 1; space < i; space++) {
+            printf(" ");
+        }
+        // Print numbers
+        for (j = i; j <= 5; j++) {
+            printf("%d", j);
+        }
+        printf("\n"); // Move to next line
+    }
+
+    return 0;
+}

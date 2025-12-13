9c84254fef2d9d2d47c5fe2c380b23acebd4506e
Aashima Khurana <aashimakhurana6@gmail.com>
2025-12-13 22:12:28 +0530

    Add files via upload

---
+++ day67.c
@@ -0,0 +1,50 @@
+//Write a program to take two sorted arrays of size m and n as input. Merge both the arrays such that the merged array is also sorted. Print the merged array.
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    
+    printf("Enter size of first array: ");
+    scanf("%d", &m);
+    int a[m];
+
+    printf("Enter elements of first sorted array:\n");
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n);
+    int b[n];
+
+    printf("Enter elements of second sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &b[i]);
+    }
+
+    int merged[m + n];
+    int i = 0, j = 0, k = 0;
+
+    // Merge process
+    while (i < m && j < n) {
+        if (a[i] <= b[j])
+            merged[k++] = a[i++];
+        else
+            merged[k++] = b[j++];
+    }
+
+    // Copy remaining elements
+    while (i < m)
+        merged[k++] = a[i++];
+
+    while (j < n)
+        merged[k++] = b[j++];
+
+    // Print merged array
+    printf("Merged sorted array:\n");
+    for (int x = 0; x < m + n; x++) {
+        printf("%d ", merged[x]);
+    }
+
+    return 0;
+}

6dad89a4c4a0b98289ab68b8c6ca308b232eb04f
Aashima Khurana <aashimakhurana6@gmail.com>
2025-12-17 20:48:01 +0530

    Add files via upload

---
+++ day76.c
@@ -0,0 +1,31 @@
+//Ask the user for a filename. Check if it exists by trying to open it in read mode. If the file pointer is NULL, print an error message; otherwise, read and display its content.
+#include <stdio.h>
+
+int main() {
+    char filename[100];
+    FILE *fp;
+    char ch;
+
+    // Ask user for filename
+    printf("Enter the filename: ");
+    scanf("%s", filename);
+
+    // Try to open file in read mode
+    fp = fopen(filename, "r");
+
+    if (fp == NULL) {
+        printf("Error: File '%s' does not exist or cannot be opened.\n", filename);
+        return 1;
+    }
+
+    // File exists, read and display its content
+    printf("Contents of '%s':\n\n", filename);
+    while ((ch = fgetc(fp)) != EOF) {
+        putchar(ch);
+    }
+
+    // Close the file
+    fclose(fp);
+
+    return 0;
+}

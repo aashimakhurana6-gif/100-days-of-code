2bac968db14c68f9037dbd14a79d00ace3467eb4
Aashima Khurana <aashimakhurana6@gmail.com>
2025-12-23 22:13:18 +0530

    Add files via upload

---
+++ day88.c
@@ -0,0 +1,18 @@
+//Print all enum names and integer values using a loop.
+#include <stdio.h>
+
+enum Role {
+    ADMIN,
+    USER,
+    GUEST
+};
+
+int main() {
+    const char *roleNames[] = {"ADMIN", "USER", "GUEST"};
+
+    for (int i = ADMIN; i <= GUEST; i++) {
+        printf("%s = %d\n", roleNames[i], i);
+    }
+
+    return 0;
+}
